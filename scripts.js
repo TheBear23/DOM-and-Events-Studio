fdf5489fe977cf25b47aea16c94db83b28d6c116
TheBear23 <ntnict@yahoo.com>
2023-02-16 15:23:26 -0600

    First changes to DOM studio

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function(){
+    const paragraph = document.getElementById('flightStatus')
+    const colorInBlack = document.getElementById('shuttleBackground')
+    const height = document.getElementById('spaceShuttleHeight')
+    const button = document.getElementById('takeoff')
+    button.addEventListener("click", function(){
+        let response = window.confirm('Confirm that the shuttle is ready for takeoff.')
+        if (response){
+            paragraph.innerHTML = 'Shuttle in flight'
+            colorInBlack.style.backgroundColor = 'blue'
+            height.innerHTML = 10000
+        }
+    })
+})
